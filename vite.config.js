6db5abe3726ff3c2fd63b14539ce8aff5b94a164
omHankare <omhankare001@gmail.com>
2025-05-15 20:58:41 +0530

    Fix config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/Om-Hankare-Portfolio",
+  base:"/Om-Hankare-Portfolio/",
 });
